d3130f3b628a49333087508cd9ebc5f2df70e96f
Leonardo Galante <lndgalante@gmail.com>
2022-09-18 12:32:44 -0300

    👌 IMPROVE: Options state. SSR values. Fix Options error. Improve responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,6 @@ import { passwordStrength } from 'check-password-strength';
   - Otras libs: Heroicons, React Range, generate-password
 
   TODO:
-  - Mobile responsive
   - Review por Hugo, Goncy, Tucu
 */
 
@@ -33,7 +32,7 @@ function Slider({ label, step, min, max, value, onChange }: SliderProps) {
     <div className='pb-4 space-y-6 group'>
       <div className='flex flex-row justify-between'>
         <span className='transition text-gray-100/40 group-hover:text-gray-100'>{label}</span>
-        <span className='text-3xl transition text-green-100/40 group-hover:text-green-100'>{value}</span>
+        <span className='text-2xl transition md:text-3xl text-green-100/40 group-hover:text-green-100'>{value}</span>
       </div>
       <Range
         step={step}
@@ -77,20 +76,27 @@ function Slider({ label, step, min, max, value, onChange }: SliderProps) {
 }
 
 type CheckboxProps = {
+  name: string;
   label: string;
   checked: boolean;
-  onChange: (checked: boolean) => void;
+  onChange: (param: { [key: string]: boolean }) => void;
 };
 
-function Checkbox({ label, checked, onChange }: CheckboxProps) {
+function Checkbox({ name, label, checked, onChange }: CheckboxProps) {
   // handlers
   function handleChange() {
-    onChange(!checked);
+    onChange({ [name]: !checked });
   }
 
   return (
     <label className='flex flex-row items-center space-x-4 cursor-pointer select-none'>
-      <input type='checkbox' className='w-5 h-5 accent-green-100' checked={checked} onChange={handleChange} />
+      <input
+        type='checkbox'
+        name={name}
+        className='w-5 h-5 accent-green-100'
+        checked={checked}
+        onChange={handleChange}
+      />
       <span>{label}</span>
     </label>
   );
@@ -149,7 +155,7 @@ function PasswordStrength({ label }: Props) {
     <div className='flex justify-between py-5 bg-gray-400 px-7'>
       <span className='text-gray-200 uppercase'>Strength</span>
       <div className='flex flex-row space-x-4'>
-        <span className='text-2xl'>{label}</span>
+        <span className='text-lg md:text-2xl'>{label}</span>
         <div className='flex space-x-2'>
           {label === 'Too weak' ? <TooWeak /> : null}
           {label === 'Weak' ? <Weak /> : null}
@@ -199,42 +205,53 @@ function Duplicate({ className }: IconProps) {
   );
 }
 
-const initialCharacterLength = 10;
+export async function getStaticProps() {
+  const initialCharacterLength = 10;
 
-const initialPassword = generatePassword.generate({
-  length: initialCharacterLength,
-  numbers: true,
-});
+  const initialPassword = generatePassword.generate({
+    length: initialCharacterLength,
+    numbers: true,
+  });
 
-const { value: initialStrength } = passwordStrength(initialPassword);
+  const { value: initialStrength } = passwordStrength(initialPassword);
 
-const Home: NextPage = () => {
+  return {
+    props: { initialCharacterLength, initialPassword, initialStrength },
+  };
+}
+
+type PageProps = {
+  initialCharacterLength: number;
+  initialPassword: string;
+  initialStrength: string;
+};
+
+const Home: NextPage<PageProps> = ({ initialCharacterLength, initialPassword, initialStrength }) => {
   // react hooks
   const [password, setPassword] = useState(initialPassword);
   const [strength, setStrength] = useState(initialStrength);
-  const [characterLength, setCharacterLength] = useState<number>(initialCharacterLength);
-
-  const [shouldIncludeUpperCaseLetters, setShouldIncludeUpperCaseLetters] = useState<boolean>(true);
-  const [shouldIncludeLowercaseLetters, setShouldIncludeLowercaseLetters] = useState<boolean>(false);
-  const [shouldIncludeNumbers, setShouldIncludeNumbers] = useState<boolean>(false);
-  const [shouldIncludeSymbols, setShouldIncludeSymbols] = useState<boolean>(false);
+  const [options, setOptions] = useState({
+    numbers: true,
+    symbols: false,
+    uppercase: false,
+    lowercase: false,
+    length: initialCharacterLength,
+  });
 
   // react-use hooks
   const { hasCopied, onCopy } = useClipboard(password);
 
   // handlers
   function handleSliderChange(values: number[]) {
-    setCharacterLength(values[0]);
+    setOptions((previousOptions) => ({ ...previousOptions, length: values[0] }));
+  }
+
+  function handleChangeOption(option: { [key: string]: boolean }) {
+    setOptions((previousOptions) => ({ ...previousOptions, ...option }));
   }
 
   function handleGeneratePassword() {
-    const password = generatePassword.generate({
-      length: characterLength,
-      numbers: shouldIncludeNumbers,
-      symbols: shouldIncludeSymbols,
-      lowercase: shouldIncludeLowercaseLetters,
-      uppercase: shouldIncludeUpperCaseLetters,
-    });
+    const password = generatePassword.generate({ ...options });
     const { value } = passwordStrength(password);
 
     setPassword(password);
@@ -243,28 +260,25 @@ const Home: NextPage = () => {
 
   // constant
   const copiedStatus = hasCopied ? 'copied' : 'not-copied';
-  const hasOptionsError = ![
-    shouldIncludeUpperCaseLetters,
-    shouldIncludeLowercaseLetters,
-    shouldIncludeNumbers,
-    shouldIncludeSymbols,
-  ].some(Boolean);
+
+  const { length, ...extraOptions } = options;
+  const hasOptionsError = !Object.values(extraOptions).some(Boolean);
 
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Password Generator</title>
         <link rel='icon' href='/favicon.ico' />
-        <meta name='description' content='Generated by create next app' />
+        <meta name='description' content='Generate strong password for your services' />
       </Head>
 
-      <main className='min-h-screen bg-gray-400 text-gray-100  text-lg font-["JetBrains_Mono"] flex items-center justify-center'>
+      <main className='min-h-screen bg-gray-400 text-gray-100 text-base md:text-lg font-["JetBrains_Mono"] flex items-center justify-center'>
         <div className='w-full max-w-lg space-y-6'>
-          <h1 className='text-2xl text-center text-gray-200'>Password Generator</h1>
+          <h1 className='text-base text-center text-gray-200 md:text-2xl'>Password Generator</h1>
 
           <div className='flex flex-row items-center justify-between px-8 py-6 bg-gray-300'>
             <motion.span
-              className='text-3xl'
+              className='text-2xl md:text-3xl'
               key={password}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
@@ -297,7 +311,7 @@ const Home: NextPage = () => {
             <Slider
               label='Character Length'
               step={1}
-              value={characterLength}
+              value={options.length}
               min={1}
               max={20}
               onChange={handleSliderChange}
@@ -305,17 +319,29 @@ const Home: NextPage = () => {
 
             <div className='space-y-3 pl-[1px]'>
               <Checkbox
+                name='uppercase'
                 label='Include Uppercase Letters'
-                checked={shouldIncludeUpperCaseLetters}
-                onChange={setShouldIncludeUpperCaseLetters}
+                checked={options.uppercase}
+                onChange={handleChangeOption}
               />
               <Checkbox
+                name='lowercase'
                 label='Include Lowercase Letters'
-                checked={shouldIncludeLowercaseLetters}
-                onChange={setShouldIncludeLowercaseLetters}
+                checked={options.lowercase}
+                onChange={handleChangeOption}
               />
-              <Checkbox label='Include Numbers' checked={shouldIncludeNumbers} onChange={setShouldIncludeNumbers} />
-              <Checkbox label='Include Symbols' checked={shouldIncludeSymbols} onChange={setShouldIncludeSymbols} />
+              <Checkbox
+                name='numbers'
+                label='Include Numbers'
+                checked={options.numbers}
+                onChange={handleChangeOption}
+              />
+              <Checkbox
+                name='symbols'
+                label='Include Symbols'
+                checked={options.symbols}
+                onChange={handleChangeOption}
+              />
               <p className='h-5 text-sm text-red-500 uppercase'>
                 {hasOptionsError ? 'Should include at least one option' : ''}
               </p>
